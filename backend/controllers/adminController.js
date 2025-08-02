7baf17b3e32643d1163903d35b525b450a33cdc7
Sanjay Palegar <sanjpalegar02@gmail.com>
2025-08-02 19:15:33 +0530

    Changed to bcrypt

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,5 +1,5 @@
 import validator from "validator"
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import { v2 as cloudinary } from "cloudinary"
 import { parse } from "dotenv"
 import doctorModel from "../models/doctorModel.js"
